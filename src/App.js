c17fc971ac7cfe0da4babe8a95eab1c5434e08ad
Kevin Trang <ktrngcode@gmail.com>
2021-11-04 14:14:30 -0400

    new form functions added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,65 @@ import NewRecipeForm from './components/NewRecipeForm'
 
 const App = () => {
   const [recipes, setRecipes] = useState([])
+  const [newTitle, setNewTitle] = useState()
+  const [newDescription, setNewDescription] = useState()
+  const [newIngredients, setNewIngredients] = useState()
+  const [newDirections, setNewDirections] = useState()
+  const [newTime, setNewTime] = useState()
+  const [newImage, setNewImage] = useState()
+  const [newComplete, setNewComplete] = useState(false)
+
+
+  //new form handlers
+  const addNewTitle = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const addNewDescription = (event) => {
+    setNewDescription(event.target.value)
+  }
+
+  const addNewIngredients = (event) => {
+    setNewIngredients(event.target.value)
+  }
+
+  const addNewDirections = (event) => {
+    setNewDirections(event.target.value)
+  }
+
+  const addNewTime = (event) => {
+    setNewTime(event.target.value)
+  }
+
+  const addNewImage = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  const addNewRecipe = (event) => {
+    event.preventDefault()
+    axios
+      .post(
+        'https://project-3-recipes.herokuapp.com/recipes',
+        {
+          title: newTitle,
+          description: newDescription,
+          ingredients: newIngredients,
+          directions: newDirections,
+          time: newTime,
+          image: newImage
+        }
+      ).then(() => {
+        axios
+          .get('https://project-3-recipes.herokuapp.com/recipes')
+          .then(() => {
+            setRecipes(response.data)
+          })
+      })
+  }
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/recipes')
+      .get('https://project-3-recipes.herokuapp.com/recipes')
       .then((response) => {
         setRecipes(response.data)
       })
@@ -18,6 +73,14 @@ const App = () => {
   return (
     <main>
       <h1>Hello World!</h1>
+      <NewRecipeForm
+        addNewTitle={addNewTitle}
+        addNewDescription={addNewDescription}
+        addNewIngredients={addNewIngredients}
+        addNewDirections={addNewDirections}
+        addNewTime={addNewTime}
+        addNewImage={addNewImage}
+      />
       {console.log(recipes)}
       <ul>
         {recipes.map((recipe) => {
