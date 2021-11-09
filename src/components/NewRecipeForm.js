1911a2fd74fbd25a2249d1c12b6986b8502109df
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-08 23:32:47 -0700

    src folder moved accidentally. fixed.

---
+++ src/components/NewRecipeForm.js
@@ -0,0 +1,25 @@
+const NewRecipeForm = (props) => {
+
+  return (
+    <div id="newFormContainer">
+      <button class="button add-recipe" onClick={props.displayModal}>Add New Recipe</button>
+      <div id="modal" className="hide">
+        <div id="modalInfo">
+          <h2>Add New Recipe</h2>
+          <button class="button" onClick={props.hideModal}>Close</button><br/><br/>
+          <form onSubmit={props.addNewRecipe}>
+            Title: <input type="text" onChange={props.addNewTitle} /><br/><br/>
+            Decription: <input type="text" onChange={props.addNewDescription} /><br/><br/>
+            Ingredients: <input type="text" onChange={props.addNewIngredients} /><br/><br/>
+            Directions: <input type="text" onChange={props.addNewDirections} /><br/><br/>
+            Time (in minutes): <input type="text" onChange={props.addNewTime} /><br/><br/>
+            Image: <input type="text" onChange={props.addNewImage} /><br/><br/>
+            <input class="button submit" type="submit" value="Submit Recipe" />
+            </form>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewRecipeForm
